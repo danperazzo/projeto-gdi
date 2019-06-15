ebee4a9da63073cd7325e44740ea1d4086215c0e
Felipe Figueiroa <figueiroa.fe@gmail.com>
2019-06-14 21:57:58 -0300

    questões de 1 a 4 feitas

---
+++ OR/checklistOR.sql
@@ -0,0 +1,29 @@
+--1., 2., 3., 4.
+CREATE OR REPLACE TYPE tp_endereco AS VARRAY(2) OF VARCHAR2(30);
+
+CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
+    nome VARCHAR2(30),
+    CPF VARCHAR2(11),
+    endr tp_endereco,
+    sexo CHAR(1)
+) NOT FINAL;
+
+CREATE OR REPLACE TYPE tp_faculdade AS OBJECT(
+    nome VARCHAR2(50),
+    num_cursos NUMBER,
+    idade NUMBER,
+    cidade VARCHAR2(30),
+);
+
+CREATE OR REPLACE TYPE tp_ref_faculdade AS TABLE OF REF tp_faculdade;
+
+CREATE OR REPLACE TYPE tp_aluno UNDER tp_pessoa(
+    curso VARCHAR2(30),
+    univ tp_ref_faculdade,
+    ano_entrada NUMBER,
+    periodo_atual NUMBER
+);
+
+-- 3.
+ 
+
