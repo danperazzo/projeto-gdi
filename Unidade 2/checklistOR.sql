106a4fddcc729b17db0f89ed63022654044d2993
gabrielamfleal <gabriela@ensinartecnologia.com.br>
2019-06-16 13:22:43 -0300

    UPDATE

--- Unidade 2/checklistOR.sql
+++ Unidade 2/checklistOR.sql
@@ -1,29 +1,29 @@
---1., 2., 3., 4.
-CREATE OR REPLACE TYPE tp_endereco AS VARRAY(2) OF VARCHAR2(30);
+-- --1., 2., 3., 4.
+-- CREATE OR REPLACE TYPE tp_endereco AS VARRAY(2) OF VARCHAR2(30);
 
-CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
-    nome VARCHAR2(30),
-    CPF VARCHAR2(11),
-    endr tp_endereco,
-    sexo CHAR(1)
-) NOT FINAL;
+-- CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
+--     nome VARCHAR2(30),
+--     CPF VARCHAR2(11),
+--     endr tp_endereco,
+--     sexo CHAR(1)
+-- ) NOT FINAL;
 
-CREATE OR REPLACE TYPE tp_faculdade AS OBJECT(
-    nome VARCHAR2(50),
-    num_cursos NUMBER,
-    idade NUMBER,
-    cidade VARCHAR2(30),
-);
+-- CREATE OR REPLACE TYPE tp_faculdade AS OBJECT(
+--     nome VARCHAR2(50),
+--     num_cursos NUMBER,
+--     idade NUMBER,
+--     cidade VARCHAR2(30),
+-- );
 
-CREATE OR REPLACE TYPE tp_ref_faculdade AS TABLE OF REF tp_faculdade;
+-- CREATE OR REPLACE TYPE tp_ref_faculdade AS TABLE OF REF tp_faculdade;
 
-CREATE OR REPLACE TYPE tp_aluno UNDER tp_pessoa(
-    curso VARCHAR2(30),
-    univ tp_ref_faculdade,
-    ano_entrada NUMBER,
-    periodo_atual NUMBER
-);
+-- CREATE OR REPLACE TYPE tp_aluno UNDER tp_pessoa(
+--     curso VARCHAR2(30),
+--     univ tp_ref_faculdade,
+--     ano_entrada NUMBER,
+--     periodo_atual NUMBER
+-- );
 
--- 3.
+-- -- 3.
  
-
+-- A CHECKLIST É A PARTIR DOS NOSSOS SCRIPTS
