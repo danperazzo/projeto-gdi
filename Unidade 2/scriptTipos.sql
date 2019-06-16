d8ee519b01c1dad50bea32a40dbb8029a41b6fa9
gabrielamfleal <gabriela@ensinartecnologia.com.br>
2019-06-15 23:43:55 -0300

    Script de tipos funcionando 10/10

--- Unidade 2/scriptTipos.sql
+++ Unidade 2/scriptTipos.sql
@@ -14,71 +14,91 @@ DROP TYPE tp_Diretor force;
 DROP TYPE tp_Genero force;
 
 CREATE OR REPLACE TYPE tp_Genero AS OBJECT (
-	Codigo_Genero INT GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
+	Codigo_Genero NUMBER,
 	Nome VARCHAR2(50),
 	Popularidade VARCHAR(50)
 ) FINAL;
+/
+-- T
 
 CREATE OR REPLACE TYPE tp_Diretor AS OBJECT (
-	Codigo_Diretor INT GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
+	Codigo_Diretor NUMBER,
 	Nome VARCHAR2(50),
     Nacionalidade VARCHAR (50),
 	Data_Nascimento DATE
 ) FINAL;
+/
+-- T
 
 CREATE OR REPLACE TYPE tp_Titulo AS OBJECT (
-	Codigo_Titulo INT GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
+	Codigo_Titulo NUMBER,
     Data DATE,
     Nome VARCHAR(50),
     Classificacao VARCHAR(50),
     Avaliacao NUMBER,
-    ref_Genero REF tp_Genero;
-    ref_Diretor REF tp_Diretor;
+    ref_Genero REF tp_Genero,
+    ref_Diretor REF tp_Diretor
 ) NOT FINAL NOT INSTANTIABLE;
+/
+-- T
 
 CREATE OR REPLACE TYPE tp_Estudio AS OBJECT (
-	Codigo_Estudio INT GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
+	Codigo_Estudio NUMBER,
     Local VARCHAR(50),
     Nome VARCHAR(50),
     Data_Lancamento DATE,
     ref_Titulo REF tp_Titulo
 ) FINAL;
+/
+-- T
 
 CREATE OR REPLACE TYPE tp_Lista AS OBJECT (
-    Codigo_Lista INT GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
+    Codigo_Lista NUMBER,
     Data_Atualizacao DATE,
     Nome VARCHAR(50)
 ) FINAL;
+/
+-- T
 
 CREATE OR REPLACE TYPE tp_Cartao_Credito AS OBJECT (
     Numero NUMBER,
     Codigo_Seguranca NUMBER,
     Bandeira VARCHAR(10)
 ) FINAL;
+/
+-- T
 
 CREATE OR REPLACE TYPE tp_Usuario AS OBJECT (
-    Email VARCHAR(40) NOT NULL,
+    Email VARCHAR(40),
     Data_Nascimento DATE,
     Nome VARCHAR(50),
-    ref_Administrador REF tp_Usuario -- auto-relacionamento
-    ref_Cartao REF tp_Cartao_Credito,
+    ref_Administrador REF tp_Usuario,
+    ref_Cartao REF tp_Cartao_Credito
 ) FINAL;
+/
+-- T
 
-CREATE OR REPLACE TYPE tp_Serie under tp_Titulo ( -- herança
+CREATE OR REPLACE TYPE tp_Serie under tp_Titulo (
     Qtd_Temporadas NUMBER
 ) FINAL;
+/
+-- T
 
-CREATE OR REPLACE TYPE tp_Filme under tp_Titulo ( -- herança
+CREATE OR REPLACE TYPE tp_Filme under tp_Titulo (
     Duracao NUMBER
 ) FINAL;
+/
+-- T
 
-CREATE OR REPLACE TYPE tp_Episodios_Serie AS OBJECT ( -- conferir
+CREATE OR REPLACE TYPE tp_Episodios_Serie AS OBJECT (
     Codigo_Titulo INT,
     Numero_Episodio NUMBER,
     Duracao NUMBER,
     Nome VARCHAR(50),
     ref_Serie REF tp_Serie
 ) FINAL;
+/
+-- T
 
 CREATE OR REPLACE TYPE tp_Adiciona AS OBJECT(
 	Email VARCHAR(40),
@@ -87,6 +107,8 @@ CREATE OR REPLACE TYPE tp_Adiciona AS OBJECT(
 	ref_Lista REF tp_Lista,
 	ref_Titulo REF tp_Titulo
 ) FINAL;
+/
+-- T
 
 CREATE OR REPLACE TYPE tp_Assiste AS OBJECT(
 	Email VARCHAR(40),
@@ -94,6 +116,8 @@ CREATE OR REPLACE TYPE tp_Assiste AS OBJECT(
     Data DATE,
 	ref_Titulo REF tp_Titulo
 ) FINAL;
+/
+-- T
 
 CREATE OR REPLACE TYPE tp_Dirige AS OBJECT(
 	Codigo_Titulo INT,
@@ -104,6 +128,8 @@ CREATE OR REPLACE TYPE tp_Dirige AS OBJECT(
     ref_Titulo REF tp_Titulo,
     ref_Diretor REF tp_Diretor
 ) FINAL;
+/
+-- T
 
 CREATE OR REPLACE TYPE tp_Premio AS OBJECT (
     Categoria VARCHAR(50),
