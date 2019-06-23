199081656d1c794ba8c008ee89fb85bb0776f421
raquel <sbsx77xavier@hotmail.com>
2019-06-23 18:53:01 -0300

    Questões 6, 7, 8, 9, 10, 23, 24, 27

--- Unidade 2/checklistOR.sql
+++ Unidade 2/checklistOR.sql
@@ -29,15 +29,17 @@
 -- A CHECKLIST É A PARTIR DOS NOSSOS SCRIPTS
 
 
---3 e 12.
+--3.
 CREATE OR REPLACE TYPE tp_va_email AS VARRAY(3) OF VARCHAR2(40);
 
+--12.
 ALTER TYPE tp_Usuario
     MODIFY ATTRIBUTE Email tp_va_email CASCADE;
 
---4 e 11.
+--4.
 CREATE OR REPLACE TYPE tp_nt_titulos AS TABLE OF VARCHAR(30);
 
+--11.
 ALTER TYPE tp_Episodios_Serie
     ADD ATTRIBUTE nome_titulo tp_nt_titulos;
 
@@ -107,9 +109,111 @@ FROM TABLE(
     WHERE U.Codigo_Titulo = 1
 )
 
+--6.
+ALTER TYPE tp_Usuario
+    ADD MEMBER FUNCTION nameToCatital RETURN VARCHAR CASCADE;
+    
+CREATE OR REPLACE TYPE BODY tp_Usuario AS
+    MEMBER FUNCTION nameToCapital RETURN VARCHAR IS
+    BEGIN
+        RETURN UPPER(SELF.Nome);
+    END;
+END;
+/
+DECLARE
+    usuarioAdm tp_Usuario;
+BEGIN
+    SELECT DEREF(ref_Administrador) INTO usuarioAdm
+    FROM tb_Usuario
+    WHERE Email = 'user1@gmail.com';
+    DBMS_OUTPUT.PUT_LINE(usuarioAdm.nameToCapital());
+END;
 
+--7 e 9 e 10.
+ALTER TYPE tp_Titulo
+    ADD MAP MEMBER FUNCTION tituloToInt RETURN NUMBER CASCADE;
+    
+ALTER TYPE tp_Serie
+    ADD OVERRIDING MAP MEMBER FUNCTION tituloToInt RETURN NUMBER CASCADE;
+CREATE OR REPLACE TYPE BODY AS
+    OVERRIDING MAP MEMBER FUNCTION tituloToInt RETURN NUMBER IS
+    BEGIN
+        RETURN Qtd_Temporadas;
+    END;
+END;
+/
+BEGIN
+    SELECT R.ref_Serie.Nome
+    FROM tb_Episodios_Serie R
+    ORDER BY DEREF(R.ref_Serie);
+END;
 
+--8.
+ALTER TYPE tp_Usuario
+    ADD ORDER MEMBER FUNCTION isOlderThan (otherUser tp_Usuario) RETURN NUMBER CASCADE;
 
+CREATE OR REPLACE TYPE BODY AS
+    ORDER MEMBER FUNCTION comp (otherUser tp_Usuario) RETURN NUMBER IS
+    BEGIN
+        RETURN otherUser.Data_Nascimento - SELF.Data_Nascimento;
+    END;
+END;
 
+DECLARE
+    userTest := tp_Usuario('usertest@gmail.com', to_date ('23/08/1971', 'dd/mm/yyyy'), 'Eduardo Fraco', (SELECT REF(U) FROM tb_Usuario U WHERE U.Email = 'user1@gmail.com'), (SELECT REF(C) FROM tb_Cartao_Credito C WHERE C.Numero = 6662));
+    userTest2 := tp_Usuario('usertest2@gmail.com', to_date ('23/08/1973', 'dd/mm/yyyy'), 'Eduardo Corno', (SELECT REF(U) FROM tb_Usuario U WHERE U.Email = 'user1@gmail.com'), (SELECT REF(C) FROM tb_Cartao_Credito C WHERE C.Numero = 6662));
+BEGIN
+    IF userTest.isOlderThan(userTest2) > 0 THEN
+        DBMS_OUTPUT.PUT_LINE('É mais velho');
+    ELSEIF userTest.isOlderThan(userTest2) < 0 THEN
+        DBMS_OUTPUT.PUT_LINE('É mais novo');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Os dois têm a mesma idade');
+    END IF
 
+END;
 
+--23.
+SELECT S.Nome
+FROM tb_Serie S
+ORDER BY S.Qtd_Temporadas;
+
+SELECT S.Nome
+FROM tb_Serie S
+GROUP BY S.Classificacao;
+
+-- Série com mais de 5 episódios
+SELECT S.ref_Serie.Nome
+FROM tb_Episodios_Serie
+GROUP BY S.ref_Serie
+HAVING COUNT (S.ref_Serie) > 5;
+
+SELECT U.Nome
+FROM tb_Usuario U
+WHERE U.Nome LIKE 'Ed%';
+
+SELECT U.Nome
+From tb_Usuario U
+WHERE U.Data_Nascimento BETWEEN to_date ('23/08/1990', 'dd/mm/yyyy') AND to_date ('23/08/2000', 'dd/mm/yyyy');
+
+--24.
+SELECT S.Nome
+FROM tb_Serie
+WHERE S.Qtd_Temporadas IN (1, 2, 3, 4, 5);
+
+SELECT S.Nome
+FROM tb_Serie
+WHERE S.Qtd_Temporadas = ALL (6, 8, 10, 20);
+
+SELECT S.Nome
+FROM tb_Serie
+WHERE S.Qtd_Temporadas = ANY (6, 8, 10, 20);
+
+--27.
+SELECT *
+FROM tb_Diretor D
+WHERE EXISTS (
+    SELECT *
+    FROM tb_Premio P
+    WHERE P.ref_Diretor = D.Codigo_Diretor;
+);
