24ad979bce67e03c696bd9a2318a80f10c3c555e
vxima <54337351+vxima@users.noreply.github.com>
2019-10-08 20:44:04 -0300

    algumas funcoes

---
+++ Checklist_Cinema.sql
@@ -0,0 +1,38 @@
+--CHECKLIST
+
+
+--13
+alter table ingresso
+modify preco default 10.0
+
+--14
+alter table generofilme
+modify genero varchar(20)
+
+--15
+alter table generofilme
+add faixa_etaria int
+
+--16
+alter table generofilme
+drop column faixa_etaria ;
+
+--17
+Select ingresso.preco , ingresso.preco + 2.0 as Novo_Preco
+from ingresso;
+
+--18
+Select AVG(i.preco)
+From ingresso i
+
+--38
+update funcionario
+set salario = ( select max(salario) from funcionario)
+    where salario < (select avg(salario) from funcionario);
+
+--44
+select f.id_func
+from funcionario f
+where f.salario  between (select min(salario) from funcionario) 
+    and (select avg(salario) from funcionario);
+
