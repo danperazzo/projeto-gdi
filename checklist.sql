5310a22da13742dc7e92839d87d134f9985761ac
Jose Roberto Fonseca Junior <jrfsj@cin.ufpe.br>
2019-10-11 17:41:14 -0300

    add 30, 46, 73

--- checklist.sql
+++ checklist.sql
@@ -55,6 +55,27 @@ Select AVG(i.preco)
 From ingresso i
 
 
+--25 Junção entre três tabelas + condição de seleção (M:N)
+
+--26 uso de inner join
+
+--27 uso de left outer join
+
+--28 uso de right outer join
+
+--29 uso de full outer join
+
+--30 Uma subconsulta com uso de ANY ou SOME
+-- seleciona todos os nomes de filmes que possuem sessoes 3d
+select nome
+from filme
+where film_id = any (select film_id from sessao where tresd = 1)
+
+--31 sunconsulta com all
+
+--32 sunconsulta com exists/not exists
+
+
 --38
 update funcionario
 set salario = ( select max(salario) from funcionario)
@@ -67,9 +88,13 @@ where f.salario  between (select min(salario) from funcionario)
     and (select avg(salario) from funcionario);
 
 
+--46 Order by com mais de dois campos
+-- seleciona todas as colunas e retorna uma tabela ordenada por nome e data de estreia
+select *
+from filme
+order by nome, estreiadata
 
 --47
-
 SELECT *
 FROM CLIENTE
 WHERE EXISTS ( SELECT * FROM ESTUDANTE WHERE ESTUDANTE.n_cadastro_Estudo = CLIENTE.n_cadastro ) OR EXISTS ( SELECT * FROM COMUM WHERE CLIENTE.n_cadastro = COMUM.n_cadastro_Comum );
@@ -213,3 +238,29 @@ CREATE OR REPLACE PROCEDURE idade_Cliente(nascimento IN DATE, idade OUT NUMBER) 
     BEGIN
         idade := EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM nascimento);
     END; 
+
+
+--72 Criação de pacote (declaração e corpo) com pelo menos dois componentes
+
+
+--73 before trigger
+create or replace trigger datatrigger
+before insert or update on filme
+for each row
+begin
+    if (:new.estreiadata < '31-jan-00') then
+        raise_application_error(-20003, 'filme deve ser mais recente que 31-jan-00');
+    end if;
+end;
+
+--74 after trigger
+
+
+--75 trigger de linha sem condicao
+
+
+--76 trigger de linha com condicao
+
+
+--77 trigger de comando
+
