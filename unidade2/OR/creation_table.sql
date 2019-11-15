ccf6966c90b430070b358d2aec256612126e4de5
danperazzo <drp@cin.ufpe.br>
2019-11-15 19:53:09 -0300

    added creation_table script and type creation script

---
+++ unidade2/OR/creation_table.sql
@@ -0,0 +1,184 @@
+
+
+--1
+DROP TABLE tb_Cliente;
+
+--2
+
+DROP TABLE tb_Estudante;
+
+--3
+DROP TABLE tb_Comum;
+
+--4
+
+DROP TABLE tb_Filho;
+
+--5
+DROP TABLE tb_Assento;
+
+--6
+
+DROP TABLE tb_Filme;
+
+
+--7
+DROP TABLE tb_Sessao;
+
+
+--8
+DROP TABLE tb_Ingresso;
+
+
+--9
+DROP TABLE tb_GeneroFilme;
+
+--10
+DROP TABLE tb_Funcionario;
+
+
+--11
+DROP TABLE tb_CarrinhoDeComida;
+
+
+--13
+DROP TABLE tb_Comida_Carrinho;
+
+
+--14
+DROP TABLE tb_Bebida_Carrinho;
+
+
+--15
+DROP TABLE tb_Trabalha;
+
+
+
+
+
+
+
+
+--1
+CREATE TABLE tb_Cliente OF tp_Cliente(
+
+    PRIMARY KEY (n_cadastro)
+
+
+);
+
+--2
+
+CREATE TABLE tb_Estudante OF tp_Estudante(
+
+  PRIMARY KEY (n_cadastro)
+);
+
+--3
+CREATE TABLE tb_Comum OF tp_Comum(
+
+  PRIMARY KEY (n_cadastro)
+
+
+
+);
+
+--4
+
+CREATE TABLE tb_Filho of tp_Filho(
+
+
+  PRIMARY KEY (n_cadastro_pai,IdFilho),
+  FOREIGN KEY (ref_tp_Cliente) REFERENCES tb_Cliente
+);
+
+--5
+CREATE TABLE tb_Assento of tp_Assento(
+  PRIMARY KEY (Fileira,Numero)
+
+);
+
+--6
+
+CREATE TABLE tb_Filme of tp_Filme(
+
+  PRIMARY KEY (Film_ID),
+  FOREIGN KEY (ref_tp_Filme) REFERENCES tb_Filme
+  
+);
+
+
+--7
+CREATE TABLE tb_Sessao of tp_Sessao(
+
+  FOREIGN KEY (ref_tp_Filme) REFERENCES tb_Filme,
+  PRIMARY KEY (Sala,Horario)
+);
+
+
+--8
+CREATE TABLE tb_Ingresso of tp_Ingresso(
+
+
+  FOREIGN KEY (Assento_Fileira,Assento_Numero) REFERENCES tb_Assento,
+  FOREIGN KEY (ref_tp_Cliente) REFERENCES tb_Cliente,
+  PRIMARY KEY (Assento_Fileira,Assento_Numero,Sessao_Horario,Sessao_Sala)
+
+);
+
+
+--9
+CREATE TABLE tb_GeneroFilme of tp_GeneroFilme(
+
+
+  FOREIGN KEY (ref_tp_Filme) REFERENCES tb_Filme,
+  PRIMARY KEY (ID_filme,genero)
+
+);
+
+--10
+CREATE TABLE tb_Funcionario of tp_Funcionario(
+
+  PRIMARY KEY (ID_func)
+
+
+);
+
+
+--11
+CREATE TABLE tb_CarrinhoDeComida of tp_CarrinhoDeComida(
+
+
+  PRIMARY KEY (ID_car)
+
+);
+
+
+--13
+CREATE TABLE tb_Comida_Carrinho of tp_Comida_Carrinho(
+
+
+  PRIMARY KEY (ID_Carrinho,comida),
+  FOREIGN KEY (ref_tp_CarrinhoDeComida) REFERENCES tb_CarrinhoDeComida
+
+);
+
+
+--14
+CREATE TABLE tb_Bebida_Carrinho of tp_Bebida_Carrinho(
+
+  PRIMARY KEY (ID_Carrinho,bebida),
+  FOREIGN KEY (ref_tp_CarrinhoDeComida) REFERENCES tb_CarrinhoDeComida
+
+);
+
+
+--15
+CREATE TABLE tb_Trabalha of tp_Trabalha(
+
+  FOREIGN KEY (ref_tp_Funcionario) REFERENCES tb_Funcionario,
+  FOREIGN KEY (ref_CarrinhoDeComida) REFERENCES tb_CarrinhoDeComida,
+  FOREIGN KEY (Sessao_hor,Sessao_sala) REFERENCES tb_Sessao,
+  PRIMARY KEY (Funcionario_id,Carrinho_come_id,Sessao_hor,Sessao_sala,Funcao)
+);
+
---
+++ unidade2/OR/type_creation.sql
@@ -0,0 +1,189 @@
+DROP TYPE tp_Cliente force;
+DROP TYPE tp_Estudante force;
+DROP TYPE tp_Comum force;
+DROP TYPE tp_Filho force;
+DROP TYPE tp_Assento force;
+DROP TYPE tp_Filme force;
+DROP TYPE tp_Sessao force;
+DROP TYPE tp_Ingresso force;
+DROP TYPE tp_GeneroFilme force;
+DROP TYPE tp_Funcionario force;
+DROP TYPE tp_CarrinhoDeComida force;
+DROP TYPE tp_Comida_Carrinho force;
+DROP TYPE tp_Bebida_Carrinho force;
+DROP TYPE tp_Trabalha force;
+
+CREATE OR REPLACE TYPE tp_Cliente AS OBJECT (
+	n_cadastro NUMBER,
+	CPF NUMBER,
+	Nascimento DATE
+) NOT FINAL;
+/
+-- T
+
+CREATE OR REPLACE TYPE tp_Estudante under tp_Cliente (
+	UNE INT
+) NOT FINAL;
+/
+-- T
+
+CREATE OR REPLACE TYPE tp_Comum under tp_Cliente(
+
+
+)NOT FINAL;
+/
+-- T
+
+CREATE OR REPLACE TYPE tp_Filho AS OBJECT(
+    n_cadastro_pai NUMBER,
+    IdFilho NUMBER,
+    Nascimento DATE,
+    Cadeirinha VARCHAR(1),
+    ref_tp_Cliente REF tp_Cliente
+)NOT FINAL;
+/
+-- T
+
+
+CREATE OR REPLACE TYPE tp_Assento AS OBJECT(
+    Fileira VARCHAR(1),
+    Numero VARCHAR(1),
+    Limpeza DATE,
+    Dbox VARCHAR(1),
+    Adaptavel VARCHAR(1)
+)NOT FINAL;
+/
+
+-- T
+
+
+CREATE OR REPLACE TYPE tp_Filme AS OBJECT(
+    Nome VARCHAR(40),
+    Film_ID INT,
+    EstreiaData DATE,
+    Seq_ID INT,
+    ref_tp_Filme  REF tp_Filme
+)NOT FINAL;
+/
+
+
+-- T
+
+CREATE OR REPLACE TYPE tp_Sessao AS OBJECT(
+    Sala VARCHAR(5),
+    Horario VARCHAR(5),
+    InicioData DATE ,
+    TresD VARCHAR(1),
+    Evento VARCHAR(40),
+    Film_ID INT,ref_tp_Filme REF tp_Filme
+    )NOT FINAL;
+/
+
+
+
+-- T
+
+CREATE OR REPLACE TYPE tp_Ingresso AS OBJECT(
+    Assento_Fileira VARCHAR(1),
+    Assento_Numero VARCHAR(1),
+    Sessao_Horario VARCHAR(5),
+    Sessao_Sala VARCHAR(5),
+    Cliente_id INT,
+    Preco FLOAT,
+    Desconto INT,
+    ref_tp_Assento REF tp_Assento,
+    ref_tp_Cliente REF tp_Cliente
+    )NOT FINAL;
+    
+/
+
+-- T
+
+CREATE OR REPLACE TYPE tp_GeneroFilme AS OBJECT(
+    genero varchar(10),
+    ID_filme INT,
+    ref_tp_Filme REF tp_Filme
+    )NOT FINAL;
+    
+/
+
+-- T
+
+CREATE OR REPLACE TYPE tp_Funcionario AS OBJECT(
+    ID_func INT,
+    Aniversario DATE,
+    Sexo VARCHAR(1),
+    Salario INT
+    )NOT FINAL;
+    
+/
+
+-- T
+
+CREATE OR REPLACE TYPE tp_CarrinhoDeComida AS OBJECT(
+    ID_car INT,
+    Revisao DATE
+    )NOT FINAL;
+    
+/
+
+-- T
+
+CREATE OR REPLACE TYPE tp_Comida_Carrinho AS OBJECT(
+    ID_Carrinho INT,
+    comida VARCHAR(10),
+    ref_tp_CarrinhoDeComida REF tp_CarrinhoDeComida
+    )NOT FINAL;
+    
+/
+
+-- T
+
+CREATE OR REPLACE TYPE tp_Bebida_Carrinho AS OBJECT(
+    ID_Carrinho INT,
+    bebida VARCHAR(10),
+    ref_tp_CarrinhoDeComida REF tp_CarrinhoDeComida
+    )NOT FINAL;
+/
+
+-- T
+
+CREATE OR REPLACE TYPE tp_Trabalha AS OBJECT(
+    Funcionario_id INT,
+    Carrinho_come_id INT,
+    Sessao_hor VARCHAR(5),
+    Sessao_sala VARCHAR(5),
+    Funcao VARCHAR(10),
+    ref_tp_Funcionario REF tp_Funcionario,
+    ref_CarrinhoDeComida REF tp_CarrinhoDeComida,
+    ref_tp_Sessao REF tp_Sessao
+    )NOT FINAL;
+/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
