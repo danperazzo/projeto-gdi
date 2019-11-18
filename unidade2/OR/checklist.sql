91d5c8659e48a7f0cbeb7990e9ca7f5071c74ba2
Gabriel <gfsca@cin.ufpe.br>
2019-11-17 23:10:49 -0300

    q26 - q30

--- unidade2/OR/checklist.sql
+++ unidade2/OR/checklist.sql
@@ -42,8 +42,55 @@ CREATE OR REPLACE TYPE tp_telefones_check AS VARRAY(10) OF VARCHAR2(15);
 
 CREATE OR REPLACE TYPE tp_telefones2_check AS TABLE OF VARCHAR(20);
 
+CREATE TABLE ntable (id NUMBER, col1 tp_telefones2_check) 
+    NESTED TABLE col1 STORE AS col1_tab;
+
+INSERT INTO nested_table VALUES (1, tp_telefones2_check('882020')); 
+INSERT INTO nested_table VALUES (2, tp_telefones2_check('102030', '203010'));
+
+SELECT * FROM ntable; -- 26
+
 -- 13.
 ALTER TYPE tp_Filho
     DROP ATTRIBUTE Nascimento DATE INVALIDATE;
     
+--27
+SELECT *
+FROM tb_Cliente C
+WHERE EXISTS (
+    SELECT *
+    FROM tb_Filho F
+    WHERE DEREF(F.ref_Cliente).n_cadastro_pai = C.n_cadastro
+);
 
+--28
+CREATE OR REPLACE TRIGGER novoCliente
+BEFORE INSERT OR UPDATE ON tb_Cliente
+FOR EACH ROW
+BEGIN
+   dbms_output.put_line('Novo cliente cadastrado');
+END novoCliente;
+
+INSERT INTO Cliente (n_cadastro, CPF,Nascimento) VALUES (6,1655,to_date ('29/07/1982', 'DD/MM/YYYY'));
+
+--29
+CREATE OR REPLACE TRIGGER maxSalario
+BEFORE INSERT OR UPDATE OR DELETE ON tb_Funcionario
+FOR EACH ROW
+BEGIN
+IF :new.Salario > 1350 THEN
+RAISE_APPLICATION_ERROR(-20222,'O teto salarial dos funcionários é de 1350 reais');
+END IF;
+END maxSalario;
+
+INSERT INTO Funcionario (ID_func, Aniversario, Sexo, Salario) VALUES
+(6,to_date ('29/08/2001', 'DD/MM/YYYY'),'F',1400);
+
+--30
+CREATE OR REPLACE TRIGGER befInUpDelFilho
+BEFORE INSERT OR UPDATE OR DELETE ON tb_Filho
+BEGIN
+RAISE_APPLICATION_ERROR(-20222,'Não é possível inserir, modificar ou deletar na tabela Filho');
+END befInUpDelFilho;
+
+INSERT INTO Filho (n_cadastro_pai,IdFilho,Nascimento,Cadeirinha) VALUES (4,4,to_date('29/10/2006', 'DD/MM/YYYY'),'0');
